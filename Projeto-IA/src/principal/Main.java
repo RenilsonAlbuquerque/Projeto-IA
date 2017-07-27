1dd466ec3fecb9100b8e162dab51cdb857c7cb93
renilsonalbuquerque@gmail.com <renilsonalbuquerque@gmail.com>
2017-07-27 16:10:31 -0300

    Implementação da leitura do arquivo.

---
+++ Projeto-IA/src/principal/Main.java
@@ -0,0 +1,11 @@
+package principal;
+
+import utils.Arquivo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Arquivo.lerArquivo();
+	}
+
+}
--- Projeto-IA/src/utils/Arquivo.java
+++ Projeto-IA/src/utils/Arquivo.java
@@ -1,10 +1,44 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 public abstract class Arquivo {
 	
-	public static void lerArquivo(){
+	public static ArrayList<Integer[]> lerArquivo(){
 		
+		ArrayList<Integer[]> base = new ArrayList<Integer[]>();
+		BufferedReader br = null;
+		FileReader fr = null;
 		
+		try{
+			fr = new FileReader("src/entradas/escolas.txt");
+			br = new BufferedReader(fr);
+			String currentLine;
+
+			while(br.ready()) {
+				currentLine = br.readLine();
+				if(!currentLine.isEmpty()){
+					base.add(Arrays.stream(currentLine.split(",")).mapToInt(Integer::parseInt).boxed().toArray( Integer[]::new ));
+				}
+			}
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				fr.close();
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return base;
 	}
 
 }
